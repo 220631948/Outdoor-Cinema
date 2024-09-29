a760314713772d15efd4ac2c60207d6b4b8fade6
Ferdi Davids <220631948@mycput.ac.za>
2024-09-29 14:48:06 +0200

    Add jakarta.validation-api dependency and create Customer and Booking classes

---
+++ src/main/java/za/ac/cput/domain/Customer.java
@@ -0,0 +1,158 @@
+package za.ac.cput.domain;
+
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Customer ID is required")
+    @Size(max = 50)
+    private String firstName;
+
+    @Email
+    @NotBlank(message = "Email is required")
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Size(max = 10)
+    private String phoneNumber;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 10, max = 25)
+    private String password;
+
+    @OneToMany(mappedBy = "customer", cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.LAZY)
+    private Set<Booking> bookings;
+
+    protected Customer() {
+    }
+
+    private Customer(Builder builder) {
+        this.id = builder.id;
+        this.firstName = builder.firstName;
+        this.email = builder.email;
+        this.phoneNumber = builder.phoneNumber;
+        this.password = builder.password;
+        this.bookings = builder.bookings;
+    }
+
+    // getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<Booking> getBookings() {
+        return bookings;
+    }
+
+    // equals and hashcode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(firstName, customer.firstName)
+                && Objects.equals(email, customer.email) && Objects.equals(phoneNumber, customer.phoneNumber)
+                && Objects.equals(password, customer.password) && Objects.equals(bookings, customer.bookings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, email, phoneNumber, password, bookings);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", bookings=" + bookings +
+                '}';
+    }
+
+    public static class Builder {
+        private Long id;
+        private String firstName;
+        private String email;
+        private String phoneNumber;
+        private String password;
+        private Set<Booking> bookings;
+
+        public Builder setFistName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setBookings(Set<Booking> bookings) {
+            this.bookings = bookings;
+            return this;
+        }
+
+        public Builder copy(Customer customer) {
+            this.id = customer.id;
+            this.firstName = customer.firstName;
+            this.email = customer.email;
+            this.phoneNumber = customer.phoneNumber;
+            this.password = customer.password;
+            this.bookings = customer.bookings;
+            return this;
+        }
+
+        public Customer build() {
+            return new Customer(this);
+        }
+    }
+}
