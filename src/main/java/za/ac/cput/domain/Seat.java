b57f3c8e5d3b6e60ae3e21eed3dcac0e28e6eb97
Ferdi Davids <220631948@mycput.ac.za>
2024-09-29 16:18:04 +0200

    Add Seat and TicketType classes

---
+++ src/main/java/za/ac/cput/domain/Seat.java
@@ -0,0 +1,17 @@
+package za.ac.cput.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "seat")
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
