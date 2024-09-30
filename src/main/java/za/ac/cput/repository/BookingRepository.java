74a5d2f2a3bf777fe6ee906118343343914edd73
Ferdi Davids <220631948@mycput.ac.za>
2024-09-30 18:57:00 +0200

    Refactor BookingRepository and BookingService classes

--- src/main/java/za/ac/cput/repository/BookingRepository.java
+++ src/main/java/za/ac/cput/repository/BookingRepository.java
@@ -1,12 +1,18 @@
 package za.ac.cput.repository;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Booking;
 
+import java.util.Date;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     Booking findByDate(String date);
+
+    boolean existsByDate(@NotNull(message = "Booking date is required") Date bookingDate);
     // Implement booking repository methods here
+    
 
 }
--- src/main/java/za/ac/cput/sevice/booking/BookingService.java
+++ src/main/java/za/ac/cput/sevice/booking/BookingService.java
@@ -3,8 +3,11 @@ package za.ac.cput.sevice.booking;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.Booking;
+import za.ac.cput.exceptions.AlreadyExistsException;
 import za.ac.cput.exceptions.ResourceNotFoundException;
 import za.ac.cput.repository.BookingRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +23,8 @@ public class BookingService {
     public Booking getBookingByDate(String date) {
         return bookRepository.findByDate(date);
     }
-    
-}
+
+    public Booking addBooking(Booking booking) {
+        return  Optional.of(booking).filter(b -> !bookRepository.existsByDate(b.getBookingDate()))
+                .map(bookRepository :: save)
+                .orElseThrow(() -> new AlreadyExistsException(booking.getDate()+" already exists"));
