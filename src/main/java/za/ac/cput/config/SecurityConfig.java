ca6658b4c4d86d45526e6a627a3cc6e489632f88
Ferdi Davids <220631948@mycput.ac.za>
2024-09-30 17:04:12 +0200

    Refactor SecurityConfig class to remove unnecessary inheritance and dependencies

--- src/main/java/za/ac/cput/config/SecurityConfig.java
+++ src/main/java/za/ac/cput/config/SecurityConfig.java
@@ -1,14 +1,12 @@
 package za.ac.cput.config;
 
 import jakarta.servlet.Filter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,7 +22,7 @@ import za.ac.cput.filter.JwtRequestFilter;
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final JwtRequestFilter jwtRequestFilter;
@@ -35,7 +33,6 @@ public class SecurityConfig extends WebSecurityConfiguration {
      * @param userDetailsService the service to load user-specific data
      * @param jwtRequestFilter the filter to handle JWT authentication
      */
-    @Autowired
     public SecurityConfig(UserDetailsService userDetailsService, JwtRequestFilter jwtRequestFilter) {
         this.userDetailsService = userDetailsService;
         this.jwtRequestFilter = jwtRequestFilter;
